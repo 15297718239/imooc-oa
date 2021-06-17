56d5ffaac7308678c155fb6779f767f817665e9e
qiyisoft@163.com <qiyisoft@163.com>
2021-06-17 11:48:05 +0800

    3-2 开发请假申请UI界面

---
+++ src/main/java/com/imooc/oa/entity/LeaveForm.java
@@ -0,0 +1,14 @@
+package com.imooc.oa.entity;
+
+import java.util.Date;
+
+public class LeaveForm {
+    private Long formId;
+    private Long employeeId;
+    private Integer formType;
+    private Date startTime;
+    private Date endTime;
+    private String reason;
+    private Date createTime;
+    private String state;
+}
