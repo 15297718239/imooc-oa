ef3e56d60a671859ba3f8a7b8f295eba71979a1b
qiyisoft@163.com <qiyisoft@163.com>
2021-06-17 12:56:45 +0800

    3-3 开发请假申请Mapper层

--- src/main/java/com/imooc/oa/entity/LeaveForm.java
+++ src/main/java/com/imooc/oa/entity/LeaveForm.java
@@ -11,4 +11,82 @@ public class LeaveForm {
     private String reason;
     private Date createTime;
     private String state;
+
+    @Override
+    public String toString() {
+        return "LeaveForm{" +
+                "formId=" + formId +
+                ", employeeId=" + employeeId +
+                ", formType=" + formType +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", reason='" + reason + '\'' +
+                ", createTime=" + createTime +
+                ", state='" + state + '\'' +
+                '}';
+    }
+
+    public Long getFormId() {
+        return formId;
+    }
+
+    public void setFormId(Long formId) {
+        this.formId = formId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Integer getFormType() {
+        return formType;
+    }
+
+    public void setFormType(Integer formType) {
+        this.formType = formType;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
