d27a68bb1e44b5324b150a626958457bd3332130
qiyisoft@163.com <qiyisoft@163.com>
2021-06-18 21:47:57 +0800

    3-6 开发请假Controller层

--- src/main/java/com/imooc/oa/controller/UserInfoServlet.java
+++ src/main/java/com/imooc/oa/controller/UserInfoServlet.java
@@ -1,7 +1,9 @@
 package com.imooc.oa.controller;
 
+import com.imooc.oa.entity.Department;
 import com.imooc.oa.entity.Employee;
 import com.imooc.oa.entity.Node;
+import com.imooc.oa.service.DepartmentService;
 import com.imooc.oa.service.EmployeeService;
 import com.imooc.oa.service.RbacService;
 import com.imooc.oa.utils.ResponseUtils;
@@ -21,6 +23,7 @@ import java.util.Map;
 public class UserInfoServlet extends HttpServlet {
     private RbacService rbacService = new RbacService();
     private EmployeeService employeeService = new EmployeeService();
+    private DepartmentService departmentService = new DepartmentService();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uid = request.getParameter("uid");
@@ -40,7 +43,9 @@ public class UserInfoServlet extends HttpServlet {
             }
         }
         Employee employee = employeeService.selectById(Long.parseLong(eid));
-        String json = new ResponseUtils().put("nodeList", treeList).put("employee",employee).toJsonString();
+        Department department = departmentService.selectById(employee.getDepartmentId());
+        String json = new ResponseUtils()
+                .put("nodeList", treeList).put("employee",employee).put("department",department).toJsonString();
         response.setContentType("application/json;charset=utf-8");
         response.getWriter().println(json);
     }
